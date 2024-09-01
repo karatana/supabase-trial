ae77a1ef9852b777e8456cb20c5bf460f621dfdd
yuki <7781183+karatana@users.noreply.github.com>
2024-09-01 05:08:04 +0000

    存在しないリンクを削除

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -13,7 +13,7 @@ export default async function ProtectedLayout({
   } = await supabase.auth.getUser();
 
   if (!user) {
-    return redirect("/login");
+    return redirect("/signup");
   }
 
   return <>{children}</>;
