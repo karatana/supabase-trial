2404a0cc7c0fa16120af955712131218b00d2fe6
yuki <7781183+karatana@users.noreply.github.com>
2024-09-01 04:11:35 +0000

    BackLinkを共通化

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import Link from 'next/link'
 import { SubmitButton } from '../../components/forms/submit-button'
 import { Input } from '@/components/forms/input'
 import { Label } from '@/components/forms/label'
 import { useFormState } from 'react-dom'
 import signUp from '@/app/actions'
+import BackLink from '@/components/BackLink'
 
 export default function Signup() {
   const initialState = { error: '' }
@@ -13,26 +13,7 @@ export default function Signup() {
 
   return (
     <div className="w-full flex-1 flex items-center h-screen sm:max-w-md justify-center gap-2 p-4">
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{' '}
-        Back
-      </Link>
+      <BackLink />
 
       <form
         className="flex flex-col w-full justify-center gap-2 text-foreground [&>input]:mb-6 max-w-md"
--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -1,5 +1,6 @@
+'use client'
+
 import { createClient } from '@/utils/supabase/server'
-import Link from 'next/link'
 import { SubmitButton } from '../../components/forms/submit-button'
 import { Input } from '@/components/forms/input'
 import { Label } from '@/components/forms/label'
@@ -8,6 +9,7 @@ import { encodedRedirect } from '@/utils/utils'
 import { cookies } from 'next/headers'
 import { isString } from '@/utils/utils'
 import { incrementLoggedInCount } from '@/database/logged_in_counts'
+import BackLink from '@/components/BackLink'
 
 export default function Verify({ searchParams }: { searchParams: Message }) {
   const verify = async (formData: FormData) => {
@@ -72,26 +74,7 @@ export default function Verify({ searchParams }: { searchParams: Message }) {
 
   return (
     <div className="w-full flex-1 flex items-center h-screen sm:max-w-md justify-center gap-2 p-4">
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{' '}
-        Back
-      </Link>
+      <BackLink />
 
       <form className="flex flex-col w-full justify-center gap-2 text-foreground [&>input]:mb-6 max-w-md">
         <h1 className="text-2xl font-medium">Verify</h1>
